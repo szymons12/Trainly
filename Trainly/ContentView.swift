ddebb6bc3d7187d764b427ae3adbcd97b14255a5
szymon_sobelski <szymon.sobelski@gmail.com>
2023-04-23 17:35:53 +0200

    Initial Commit

---
+++ Trainly/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Trainly
+//
+//  Created by szymon on 23/04/2023.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Trainly/TrainlyApp.swift
@@ -0,0 +1,17 @@
+//
+//  TrainlyApp.swift
+//  Trainly
+//
+//  Created by szymon on 23/04/2023.
+//
+
+import SwiftUI
+
+@main
+struct TrainlyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
